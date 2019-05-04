3ae396d72570b791230a8da87719952b14dbcaba
Marcus Runge <marcus.runge@outlook.com>
2019-05-04 14:06:26 +0200

    ADDED DeltaLoopUnitTest

---
+++ DeltaLoopUnitTest/DeltaLoopUnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeltaLoopUnitTest
+{
+    [TestClass]
+    public class DeltaLoopUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
