196cfdc80021dcc7cc0edb60c17c3f03f4b53efc
Marcus Runge <marcus.runge@outlook.com>
2019-05-13 20:19:30 +0200

    TestSocket

--- EchoSocket/EchoSocket.cpp
+++ EchoSocket/EchoSocket.cpp
@@ -1,8 +1,89 @@
 ﻿#include "pch.h"
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "EchoSocket.h"
-#include <winsock.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <tchar.h>
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT 27015
 using namespace Platform;
 int TestSocket(DWORD socket) 
-{	
-	return EXIT_SUCCESS;
+{		
+	int iResult;
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct sockaddr_in clientService;
+	int recvbuflen = DEFAULT_BUFLEN;
+	char* sendbuf = "Client: sending data test";
+	char recvbuf[DEFAULT_BUFLEN] = "";
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR) {
+		wprintf(L"WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ConnectSocket = (SOCKET)socket;
+	if (ConnectSocket == INVALID_SOCKET) {
+		wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+		
+	clientService.sin_family = AF_INET;
+	clientService.sin_addr.s_addr = inet_addr("127.0.0.1");
+	clientService.sin_port = htons(DEFAULT_PORT);
+		
+	iResult = connect(ConnectSocket, (SOCKADDR*)& clientService, sizeof(clientService));
+	if (iResult == SOCKET_ERROR) {
+		wprintf(L"connect failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+		
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		wprintf(L"send failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	//printf("Bytes Sent: %d\n", iResult);
+	
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		wprintf(L"shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+		
+	/*do {
+
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0)
+			wprintf(L"Bytes received: %d\n", iResult);
+		else if (iResult == 0)
+			wprintf(L"Connection closed\n");
+		else
+			wprintf(L"recv failed with error: %d\n", WSAGetLastError());
+
+	} while (iResult > 0);*/
+
+
+	
+	iResult = closesocket(ConnectSocket);
+	if (iResult == SOCKET_ERROR) {
+		wprintf(L"close failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	WSACleanup();
+	return 0;
 }
