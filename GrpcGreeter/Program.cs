f19132cb9fe044f240a10c538639c2b2e28b7c7d
Marcus Runge <marcus.runge@outlook.com>
2020-11-04 14:41:57 +0100

    UPDATED gRPC

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -18,7 +18,7 @@ namespace GrpcGreeter
                 webBuilder.ConfigureKestrel(options =>
                 {
                     // Setup a HTTP/2 endpoint without TLS.
-                    options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http2);
+                    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
                 });
                 webBuilder.UseStartup<Startup>();
             });
--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -11,7 +11,7 @@ namespace GrpcGreeterClient
             // This switch must be set before creating the GrpcChannel/HttpClient.
             AppContext.SetSwitch( "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:5001");
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:5000");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
