a7531e95010e5a5f3bb593d1b5721219a1d55165
Marcus Runge <marcus.runge@outlook.com>
2020-11-04 14:49:29 +0100

    ListenAnyIP

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -18,7 +18,7 @@ namespace GrpcGreeter
                 webBuilder.ConfigureKestrel(options =>
                 {
                     // Setup a HTTP/2 endpoint without TLS.
-                    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+                    options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
                 });
                 webBuilder.UseStartup<Startup>();
             });
