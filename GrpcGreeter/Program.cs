8a9ee3d98839c7fed31e1e72d6a6931687c59ceb
Marcus Runge <marcus.runge@outlook.com>
2020-11-04 09:05:55 +0100

    REFACTORED gPRC to Http2UnencryptedSupport

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace GrpcGreeter
@@ -17,11 +13,14 @@ namespace GrpcGreeter
 
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureKestrel(options =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    // Setup a HTTP/2 endpoint without TLS.
+                    options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http2);
                 });
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -8,8 +8,10 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch( "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
